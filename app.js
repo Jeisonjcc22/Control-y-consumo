50ad58a351a51f9e7c4b3f85e5064654c4aa1440
Jeisonjcc22 <jeisonjcc2263@gmail.com>
2025-01-20 14:49:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//Ficha 2721427 Analisis y desarrollo de software API GA7-220501096-AA5-EV02
+//Proyecto CCM Control y consumo de material
+//Integrantes: Caren Carillo
+//             Cristhian Rocha
+//             Jeison Contreras
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static('public')); // Carpeta donde están los archivos estáticos
+
+
+// Importar rutas
+const authRoutes = require('./routes/auth');
+const registroRoutes = require('./routes/registros');
+
+// Ruta para el inicio
+app.get('/inicio', (req, res) => {
+    res.sendFile(__dirname + '/public/inicio.html'); // Redirige a inicio.html en la carpeta public
+});
+
+
+// Usar rutas
+app.use('/auth', authRoutes);
+app.use('/registros', registroRoutes);
+
+// Servidor
+app.listen(3000, () => {
+    console.log(`Servidor escuchando en http://localhost:3000`);
+});
